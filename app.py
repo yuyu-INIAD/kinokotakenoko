00909ae13a139d69c3f99ad60476a57f347582fe
yu1453 <s1f102102380@iniad.org>
2021-06-28 15:47:24 +0900

    push

--- app.py
+++ app.py
@@ -11,8 +11,21 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    global kinoko_count,takenoko_count,messages
+    if request.form.get("item") == "kinoko":
+        kinoko_count += 1
+    elif request.form.get("item") == "takenoko":
+        takenoko_count += 1
+    messages.append(request.form.get("message"))
+    if len(messages) > 3:
+        messages = messages[-3:]
+    print(request.form,kinoko_count,takenoko_count)
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+    message_hrml = ""
+    for i in range(len(messages)):
+        message = messages[i]
+        message_hrml += '<div class="alert {1}" role="alert">{0}</div>\n'.format(message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
