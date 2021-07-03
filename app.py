4bf93a33d862ff3ce5a514194d750950ea03fdb8
yu1453 <s1f102102380@iniad.org>
2021-07-03 18:20:52 +0900

    a

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+import re
 kinoko_count = 3
 takenoko_count = 5
 messages = ["kinoko is wonderful!", "takenoko is awesome!"]
@@ -22,10 +23,16 @@ def answer():
     print(request.form,kinoko_count,takenoko_count)
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
-    message_hrml = ""
+    message_html = ""
     for i in range(len(messages)):
         message = messages[i]
-        message_hrml += '<div class="alert {1}" role="alert">{0}</div>\n'.format(message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+        message = re.sub(r'&', r'&amp', message)
+        message = re.sub(r'<', r'&lt', message)
+        message = re.sub(r'<', r'&gt', message)
+        message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
+        message = re.sub(r'(0[789]0)-\d+-\d+', r'\1-****_****', message)
+        message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
+            message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
